bd97f0fe34cc7ecfb4dd23f59e523f10fc6880ae
AsrieltheGoat <airdroid271@gmail.com>
2023-06-25 00:01:50 +0700

    Remade everything

--- index.js
+++ index.js
@@ -1,104 +1,178 @@
-const dotenv = require("dotenv").config();
-const express = require("express");
-const app = express();
-const port = 3000;
 const ytdl = require("ytdl-core");
 const cp = require("child_process");
 const ffmpeg = require("ffmpeg-static");
+
+// Set up express server
+const express = require("express");
+const app = express();
+const port = 3000;
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/public/index.html");
 });
 
-app.get("/youtube", (req, res) => {
-    if (req.query.url) {
-        if (req.query.info == "true") {
-            ytdl.getInfo(req.query.url).then((info) => {
-                res.json({
-                    title: info.videoDetails.title,
-                    description: info.videoDetails.description,
-                    duration: info.videoDetails.lengthSeconds,
-                    url: info.videoDetails.video_url,
-                    id: info.videoDetails.videoId,
-                    published: info.videoDetails.publishDate,
-                    views: info.videoDetails.viewCount,
-                    // download: info.formats[0].url,
-                    thumbnail: info.videoDetails.thumbnails[0].url,
-                    owner: info.videoDetails.author.name,
-                    owner_url: info.videoDetails.author.user_url,
-                    owner_profile_picture: info.videoDetails.author.thumbnails[0].url,
-                    owner_subscribers: info.videoDetails.author.subscriber_count,
-                    owner_verified: info.videoDetails.author.verified,
+// Url, info=true/false, format=audio/video
+app.get("/youtube", async (req, res) => {
+    const url = req.query.url;
+    const info = req.query.info;
+    const format = req.query.format;
+    const quality = req.query.quality;
+
+    if (ytdl.validateURL(url) || ytdl.validateID(url)) {
+        if (info == "true") {
+            // Get video info
+            const info = await ytdl.getInfo(url);
+            return res.send(info);
+        }
+
+        if (format) {
+            // Get the video name
+            const info = await ytdl.getInfo(url);
+            const videoName = info.videoDetails.title;
+
+            if (format == "audio") {
+                // Pipe audio to ffmpeg
+                const audio = ytdl(url, {
+                    filter: "audioonly",
+                    highWaterMark: 1 << 25,
                 });
-            });
-        } else {
-            if (req.query.type == "audio") {
-                
-                ytdl(req.query.url, { filter: "audioonly" }).pipe(res);
+
+                // Set the file name to the video name
+                const fileName = `${videoName}.mp3`;
+
+                // Encode file name
+                const encodedFileName = encodeURIComponent(fileName);
+                // Set the content disposition header to force download
+                res.setHeader(
+                    "Content-Disposition",
+                    `attachment; filename="${encodedFileName}"`
+                );
+
+                // Pipe audio to ffmpeg
+                const ffmpegProcess = cp.spawn(ffmpeg, [
+                    "-i",
+                    "pipe:0",
+                    "-f",
+                    "mp3",
+                    "-ab",
+                    "192000",
+                    "-vn",
+                    "pipe:1",
+                ]);
+
+                // Pipe ffmpeg output to response
+                audio.pipe(ffmpegProcess.stdin);
+                ffmpegProcess.stdout.pipe(res);
+
+                // Kill ffmpeg process if the user closes the connection
+                res.on("close", () => {
+                    ffmpegProcess.kill("SIGKILL");
+                });
+
+                return;
             }
 
-            if (req.query.type == "video" || !req.query.type) {
-                ytdl.getInfo(req.query.url).then((info) => {
-                    var videoName = info.videoDetails.title + ".mp4";
+            if (format == "video") {
+                // check if quality is highest, lowest, highestvideo, lowestvideo
+                if (
+                    quality == "highest" ||
+                    quality == "lowest" ||
+                    quality == "highestvideo" ||
+                    quality == "lowestvideo"
+                ) {
+                    // Pipe video to ffmpeg
+                    const video = ytdl(url, {
+                        filter: "videoonly",
+                        quality: quality,
+                        highWaterMark: 1 << 25,
+                    });
 
-                    res.header(
-                        `Content-Disposition`,
-                        `attachment; filename=${videoName}`
+                    // Pipe audio to ffmpeg
+                    const audio = ytdl(url, {
+                        filter: "audioonly",
+                        highWaterMark: 1 << 25,
+                    });
+
+                    // Set the file name to the video name
+                    const fileName = `${videoName}.mp4`;
+
+                    // Encode file name
+                    const encodedFileName = encodeURIComponent(fileName);
+                    // Set the content disposition header to force download
+                    res.setHeader(
+                        "Content-Disposition",
+                        `attachment; filename="${encodedFileName}"`
                     );
 
-                    if (req.query.quality == "low" || !req.query.quality) {
-                        ytdl(req.query.url).pipe(res);
-                    }
-
-                    if (req.query.quality == "high") {
-                        ytdl(req.query.url, { quality: "highest" }).pipe(res);
-                    }
-                })
-            }
-
-            if (req.query.type == "both") {
-                ytdl.getInfo(req.query.url).then((info) => {
-                    var videoName = info.videoDetails.title + ".mp4";
-
+                    // Pipe ffmpeg output to response
                     const ffmpegProcess = cp.spawn(
                         ffmpeg,
                         [
-                            '-i', `pipe:3`,
-                            '-i', `pipe:4`,
-                            '-map', '0:v',
-                            '-map', '1:a',
-                            '-c:v', 'copy',
-                            '-c:a', 'libmp3lame',
-                            '-crf', '27',
-                            '-preset', 'veryfast',
-                            '-movflags', 'frag_keyframe+empty_moov',
-                            '-f', 'mp4',
-                            '-loglevel', 'error',
-                            '-'
+                            "-i",
+                            `pipe:3`,
+                            "-i",
+                            `pipe:4`,
+                            "-map",
+                            "0:v",
+                            "-map",
+                            "1:a",
+                            "-c:v",
+                            "libx264",
+                            "-c:a",
+                            "aac",
+                            "-crf",
+                            "27",
+                            "-preset",
+                            "veryfast",
+                            "-movflags",
+                            "frag_keyframe+empty_moov",
+                            "-f",
+                            "mp4",
+                            "-loglevel",
+                            "error",
+                            "-",
                         ],
                         {
                             stdio: ["pipe", "pipe", "pipe", "pipe", "pipe"],
                         }
                     );
-                    res.header(
-                        `Content-Disposition`,
-                        `attachment; filename=${videoName}`
+
+                    video.pipe(ffmpegProcess.stdio[3]);
+                    audio.pipe(ffmpegProcess.stdio[4]);
+                    ffmpegProcess.stdio[1].pipe(res); // Pipe ffmpeg output to response
+
+                    let ffmpegLogs = "";
+
+                    ffmpegProcess.stdio[2].on("data", (chunk) => {
+                        ffmpegLogs += chunk.toString();
+                    });
+
+                    ffmpegProcess.on("exit", (exitCode) => {
+                        if (exitCode === 1) {
+                            console.error(ffmpegLogs);
+                        }
+                    });
+
+                    return;
+                }
+                return res
+                    .status(400)
+                    .send(
+                        "Invalid quality<br/>Valid qualities: highest, lowest, highestvideo, lowestvideo"
                     );
-                    ytdl(req.query.url, { quality: "highestvideo" }).pipe(
-                        ffmpegProcess.stdio[3]
-                    );
-                    ytdl(req.query.url, { filter: "audioonly" }).pipe(
-                        ffmpegProcess.stdio[4]
-                    );
-                    ffmpegProcess.stdio[1].pipe(res);
-                });
             }
         }
-    } else {
-        res.send("No URL provided");
+        return res
+            .status(400)
+            .send("Invalid format<br/>Valid formats: audio, video");
     }
+    return res
+        .status(400)
+        .send(
+            "Invalid URL<br/>Enter an valid YouTube URL/ID<br/>Example: https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+        );
 });
 
 app.listen(port, () => {
-    // console.log(`Listening on port ${port}!`);
+    console.log(`Listening on port ${port}!`);
 });
